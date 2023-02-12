aa2b9061e720812ad54777bfdcef7325473acd50
alexgtang <agt2120@columbia.edu>
2023-02-12 15:55:27 -0500

    added values section

--- frontend/src/components/Home/HomeBody.jsx
+++ frontend/src/components/Home/HomeBody.jsx
@@ -11,6 +11,9 @@ import TempImg3 from "../../static/images/fruit.png";
 import TempImg4 from "../../static/images/kiwi.png";
 import TempImg5 from "../../static/images/raspberry.png";
 import TempImg6 from "../../static/images/orange.png";
+import TempImg7 from "../../static/images/italian.png";
+import TempImg8 from "../../static/images/apple.png";
+import TempImg9 from "../../static/images/tart.png";
 import Divider from "../../static/images/divider.png";
 
 class HomeBody extends Component {
@@ -104,12 +107,75 @@ class HomeBody extends Component {
         </div>
         <div className="secondary_block">
           <div className="container title">
-            <h1 style={{ fontSize: 40, marginBottom: 0, color: 'black' }} className="bolder">
-              Who are we?
+            <h1
+              style={{ fontSize: 40, marginBottom: 0, color: "black" }}
+              className="bolder"
+            >
+              Values
             </h1>
-            <h2 style={{ fontSize: 20, marginTop: 10, color: 'black' }} className="my-3">
+            <h2
+              style={{ fontSize: 20, marginTop: 10, color: "black" }}
+              className="my-3"
+            >
               Developed by college students for college students.
             </h2>
+          </div>
+        </div>
+        <div
+          className="container mx-5 my-5"
+          style={{ justifyContent: "center", alignContent: "center" }}
+        >
+          <div className="d-flex mt-3">
+            {/* very temporary for now, just placeholders, need to create separate card function */}
+            <div
+              className="me-5 ms-5"
+              style={{ justifyContent: "center", alignContent: "center" }}
+            >
+              <Card.Img
+                className="d-flex small-card-size"
+                src={TempImg7}
+                alt=""
+              />
+              <div
+                className="me-2 my-3"
+                style={{ justifyContent: "center" }}
+              >
+                Something about how we very much value the importance of trees.
+              </div>
+            </div>
+            <div
+              className="me-5"
+              style={{ justifyContent: "center", alignContent: "center" }}
+            >
+              <Card.Img
+                className="d-flex small-card-size"
+                src={TempImg8}
+                alt=""
+              />
+              <div
+                className="me-2 my-3"
+                style={{ justifyContent: "center" }}
+              >
+                Something about how we very much value the importance of apples.
+              </div>
+            </div>
+            <div
+              className="me-5"
+              style={{ justifyContent: "center", alignContent: "center" }}
+            >
+              <Card.Img
+                className="d-flex small-card-size"
+                src={TempImg9}
+                alt=""
+              />
+              <div
+                className="me-3 my-3"
+                style={{ justifyContent: "center" }}
+              >
+                Something about how we very much value the importance of
+                mushrooms.
+              </div>
+            </div>
           </div>
         </div>
       </div>
